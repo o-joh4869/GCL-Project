048430346ed882ab6bbc164c55fa5d02282e5449
mattyan1053 <h.matsumaruka14161@gmail.com>
2019-03-27 18:15:53 +0900

    [Input::Mouse]矩形領域に対するマウス判定メソッド、及びマウスの座病フレーム差分を取得するgetDelta()関数を追加

--- Project/Code/Input/Mouse.h
+++ Project/Code/Input/Mouse.h
@@ -50,10 +50,83 @@ namespace Input {
 		// 関数オブジェクトの条件を満たす座標で、buttonTypeで指定したマウスのボタンがbuttonStateに指定した状態変化を何度したか取得する
 		int getButtonChangeCount(std::function<bool(const Vector2<int>)> func, int buttonType = MOUSE_INPUT_LEFT, int buttonState = MOUSE_INPUT_LOG_DOWN) const;
 
+		// １フレーム前からのマウス座標の差分を返す
+		Vector2<int> getDelta() const {
+			return mPoint - mPrePoint;
+		}
+
+		// 左上 p1 、右下 p2 の矩形の中にマウス座標が含まれているかを返す
+		bool isInside(Vector2<int> p1, Vector2<int> p2) const {
+			return p1.x <= mPoint.x && mPoint.x < p2.x && p1.y <= mPoint.y && mPoint.y < p2.y;
+		}
+
+		// マウスボタン状況
+		struct ButtonState {
+
+			int buttonType;
+
+			int pressedDuration;
+			bool clicked;
+			bool pressed;
+			bool released;
+
+			ButtonState(int _buttonType) :
+				buttonType(_buttonType),
+				pressedDuration(0),
+				clicked(false),
+				pressed(false),
+				released(false) {}
+
+			void update();
+
+		};
+
+		// 左上座標 p1 、右下座標 p2 の矩形範囲内でマウス左ボタンがクリックされたかどうかを返す
+		bool leftClicked(Vector2<int> p1 = { 0, 0 }, Vector2<int> p2 = { 960, 540 }) const {
+			return isInside(p1, p2) && mMouseL.clicked;
+		}
+		// 左上座標 p1 、右下座標 p2 の矩形範囲内でマウス左ボタンが押されているかどうかを返す
+		bool leftPressed(Vector2<int> p1 = { 0, 0 }, Vector2<int> p2 = { 960, 540 }) const {
+			return isInside(p1, p2) && mMouseL.pressed;
+		}
+		// 左上座標 p1 、右下座標 p2 の矩形範囲内でマウス左ボタンが離されたかどうかを返す
+		bool leftReleased(Vector2<int> p1 = { 0, 0 }, Vector2<int> p2 = { 960, 540 }) const {
+			return isInside(p1, p2) && mMouseL.released;
+		}
+
+		// 左上座標 p1 、右下座標 p2 の矩形範囲内でマウス右ボタンがクリックされたかどうかを返す
+		bool rightClicked(Vector2<int> p1 = { 0, 0 }, Vector2<int> p2 = { 960, 540 }) const {
+			return isInside(p1, p2) && mMouseR.clicked;
+		}
+		// 左上座標 p1 、右下座標 p2 の矩形範囲内でマウス右ボタンが押されているかどうかを返す
+		bool rightPressed(Vector2<int> p1 = { 0, 0 }, Vector2<int> p2 = { 960, 540 }) const {
+			return isInside(p1, p2) && mMouseR.pressed;
+		}
+		// 左上座標 p1 、右下座標 p2 の矩形範囲内でマウス右ボタンが離されたかどうかを返す
+		bool rightReleased(Vector2<int> p1 = { 0, 0 }, Vector2<int> p2 = { 960, 540 }) const {
+			return isInside(p1, p2) && mMouseR.released;
+		}
+
+		// マウス左ボタンが何フレーム押されているかを返す
+		int leftPressedDuration(Vector2<int> p1 = { 0, 0 }, Vector2<int> p2 = { 960, 540 }) const {
+			return mMouseL.pressedDuration;
+		}
+		// マウス右ボタンが何フレーム押されているかを返す
+		int rightPressedDuration(Vector2<int> p1 = { 0, 0 }, Vector2<int> p2 = { 960, 540 }) const {
+			return mMouseR.pressedDuration;
+		}
+
 	private:
 		static Vector2<int> mPoint;
+		// １フレーム前のマウス座標
+		static Vector2<int> mPrePoint;
 
 		static std::vector<ButtonInfo> mButtonLog;
+
+		// 左ボタン
+		static ButtonState mMouseL;
+		// 右ボタン
+		static ButtonState mMouseR;
 		
 		friend Manager;
 	};
